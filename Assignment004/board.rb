106a06f28ebb9f7baf616771163edf264609c1ab
Malav Desai <malav1410@gmail.com>
2014-04-22 12:15:19 +0530

    commenting and some changes has been done in Assignment-4, still refactoring is left

--- Assignment004/board.rb
+++ Assignment004/board.rb
@@ -24,7 +24,7 @@ class Board
   end
 
 private
-
+  # Two new players are created every time when new game starts
   def create_player(name, symbol)
     player_name = name
     player_symbol = symbol
@@ -35,13 +35,13 @@ private
   def start_game_logic
     display_game_board
     n = 0
-    while n < 6 do
+    while n < 5 do
       position_entered_by_player1
       display_game_board
       if n > 1
         if check_result(@player1_arr.sort!) == true
           puts "#{@player1.name.capitalize} wins"
-          end_game_logic
+          return
         end
       end
       position_entered_by_player2
@@ -49,75 +49,76 @@ private
       if n > 1
         if check_result(@player2_arr.sort!) == true
           puts "#{@player2.name.capitalize} wins"
-          end_game_logic
+          return
         end
       end
       n += 1
     end
   end
 
-  def end_game_logic
-    puts "Play again?(y/n)"
-    play_again = gets.chomp
-    if play_again == "y"
-      start
-    elsif play_again == "n"
-      puts "thanks for playing"
-      exit
-    else
-      end_game_logic
-    end
-  end
-
   # Display who wins
   def check_result(position_arr)
     @result_arr.each do |lucky_arr|
+      # Compares two array with '&' operator
+      # example: [1,3,6,8,9] & [3,8,9] => [3,8,9], use of & operator  
       if position_arr & lucky_arr == lucky_arr
         return true
-      else
-        false
       end
     end
+    # Player1 have maximum 5 moves and player2 have maximum 4 moves
+    # If result not come at the end of total 9 moves than match is draw. 
+    if @player1_arr.count == 5 && @player2_arr.count == 4
+      puts "Match is draw!!"
+      return
+    end
   end
+  
   # Player 1
+  # Logic to store position entered by player
   def position_entered_by_player1
     puts "#{@player1.name.capitalize}'s turn(X):"
-    player1_position = gets.chomp.to_i
-    if is_position_valid?(player1_position) == true 
+    position = gets.chomp
+    player1_position = position.to_i
+    if is_position_valid?(position) == true 
       @board_value_hash[player1_position] = "X"
       @player1_arr.push player1_position
     else
-      puts "Enter valid position"
+      puts "Enter valid position from '1-9'\n"
       position_entered_by_player1
     end
   end
 
   # Player 2
+  # Logic to store position entered by player
   def position_entered_by_player2
     puts "#{@player2.name.capitalize}'s turn(O):"
-    player2_position = gets.chomp.to_i
-    if is_position_valid?(player2_position) == true 
+    position = gets.chomp
+    player2_position = position.to_i
+    if is_position_valid?(position) == true 
       @board_value_hash[player2_position] = "O"
       @player2_arr.push player2_position
     else
-      puts "Enter valid position"
+      puts "Enter valid position from '1-9'\n"
       position_entered_by_player2
     end
   end
 
+  # When player enter any value, this will check if its valid value for game or not.
   def is_position_valid?(position)
-    if /(^[1-9]$)/.match(position.to_s) != nil  
-      if @board_value_hash[position] != nil
-        puts "The position is already entered, please enter again"
-        return false
-      else
+    # Matching starting point as number and ending point as number and consider only one digit in Regexp condition.
+    if /(^[1-9]$)/.match(position) != nil  
+      # If position is already occupied by "X" or "O", can't override that position again.
+      if @board_value_hash[position.to_i] == nil
         return true
+      else
+        puts "The position is already entered, please enter different position\n"
       end
     else
       return false
     end
   end
 
+  # Main game board which contain value of @board_value_hash keys's value, which is defined in initialize method
   def display_game_board
     puts "   #{@board_value_hash[1]}   |   #{@board_value_hash[2]}   |   #{@board_value_hash[3]}   "
     puts "-----------------------"
@@ -126,6 +127,8 @@ private
     puts "   #{@board_value_hash[7]}   |   #{@board_value_hash[8]}   |   #{@board_value_hash[9]}   "
     puts "\n\n"
   end
+  
+  # Instruction related to game
   def instructions
     puts "-------------------------------"
     puts "Welcome to the TIC-TAC-TOE game"
--- Assignment004/main.rb
+++ Assignment004/main.rb
@@ -1,4 +1,10 @@
 load 'board.rb'
 
-board = Board.new
-board.start
+begin
+  board = Board.new
+  board.start
+  puts "Play again!!?? (y/n)"
+  play_again = gets.chomp
+end while play_again != "n"
+
+puts "Thanks for playing!!"
